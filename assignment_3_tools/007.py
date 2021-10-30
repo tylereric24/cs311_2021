ea6362768d0fc3a3ec97bf93853a0a68bda3c76c
Eric Tyler <83565207+tylereric24@users.noreply.github.com>
2021-10-30 15:33:34 -0700

    Update 007.py

--- assignment_3_tools/007.py
+++ assignment_3_tools/007.py
@@ -1,5 +1,6 @@
 import argparse
 import random
+import json
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
@@ -9,9 +10,47 @@ if __name__ == "__main__":
 
     args = parser.parse_args()
 
-    #print(args.last_opponent_move)
+def __init__(self):
+self.N = self.C = self.D = self.ci = self.di = 0
+self.other_strategy = "confess"
 
-    print( random.choice(['confess', 'silent']) )
+def process_results(self, main_strategy, other_strategy):
+    pass
+
+"""
+
+"""
+
+def pick_move
+# N = number of rounds
+# C is number of times ive confessed
+# D is number of times ive stayed silent
+# ci is number of times in a row ive confessed
+# di is number of times in a row ive stayed silent
+
+if self.di > 5:
+    print("confess")
+
+else:
+    random.randint(0,3)
+    if r == 0:
+    return self.otherstrategy
+
+    else:
+        print("silent")
+
+def process_results(self, main_strategy, other_strategy):
+    self.N += 1
+    if main_strategy == True:
+        self.C += 1
+        self.ci += 1
+        self.di = 0
+
+    elif main_strategy == False:
+        self.D += 1
+        self.di += 1
+        self.ci = 0
+        self.other_strategy = other_strategy
     
 
 
