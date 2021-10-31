f310e0bea43cb2996e9ecfa14a5678998b9ade5a
Eric Tyler <83565207+tylereric24@users.noreply.github.com>
2021-10-31 13:57:42 -0700

    Update 007.py

--- assignment_3_tools/007.py
+++ assignment_3_tools/007.py
@@ -6,7 +6,7 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument('--init', help='called when new game')
     parser.add_argument('--iterations', help='number of iterations in game')
-    parser.add_argument('--last_opponent_move', help='last opponent move', action="store_true")
+    parser.add_argument('--last_opponent_move', help='last opponent move')
 
     args = parser.parse_args()
 
