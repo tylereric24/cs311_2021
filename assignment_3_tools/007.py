e295bb14494fc2754522e25854ee5d31d9056c28
Eric Tyler <83565207+tylereric24@users.noreply.github.com>
2021-10-31 21:28:12 -0700

    Update 007.py

--- assignment_3_tools/007.py
+++ assignment_3_tools/007.py
@@ -7,19 +7,6 @@ from os.path import exists
 opp_moves_history = "opp_moves_history.json"
 parameter_storage = "parameter_storage.json"
 
-if __name__ == "__main__":
-
-    
-    parser = argparse.ArgumentParser()
-    parser.add_argument('--init', help='called when new game')
-    parser.add_argument('--iterations', help='number of iterations in game')
-    parser.add_argument('--last_opponent_move', help='last opponent move')
-    args = parser.parse_args()
-
-   
-    is_new_game = args.init
-    iterations = args.iterations
-    opponents_last_move = args.last_opponent_move
 
 
 
@@ -74,6 +61,19 @@ def print_data():
     print(opponent_history)
     print("\n")
 
+if __name__ == "__main__":
+
+    
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--init', help='called when new game')
+    parser.add_argument('--iterations', help='number of iterations in game')
+    parser.add_argument('--last_opponent_move', help='last opponent move')
+    args = parser.parse_args()
+
+   
+    is_new_game = args.init
+    iterations = args.iterations
+    opponents_last_move = args.last_opponent_move
 
 
 
@@ -127,7 +127,7 @@ def print_data():
     else:
 
         
-        if parameters["iterations"] == 50:
+        if parameters["iterations"] < 50:
 
             r == random.randint(0,3)
 
@@ -153,8 +153,9 @@ def print_data():
 
                    
                     try:
-
-                        if random.randint(0, parameters["spill_the_beans_odds"]) == 0:
+                        t = random.randint(0,1,2,3,4,5,6,7,8,9,10)
+                        
+                        if t == 0,2,4,6,8,10:
                             print("confess")
                         else:
                             print("silent")
