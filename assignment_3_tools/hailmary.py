f5ba3a862d3f6f18d9ce358468adb8456a71af8a
Eric Tyler <83565207+tylereric24@users.noreply.github.com>
2021-11-01 12:04:57 -0700

    Update hailmary.py

--- assignment_3_tools/hailmary.py
+++ assignment_3_tools/hailmary.py
@@ -3,21 +3,17 @@ import random
 import json
 from os.path import exists
 
-
 opp_moves_history = "opp_moves_history.json"
 parameter_storage = "parameter_storage.json"
-
-
-
-
 
 parameters = {
    
     "anger": True,
     "grudgelength": 4,
     "spill_the_beans": True,
-    "spill_the_beans_odds": 200,
+    "spill_the_beans_odds": 50,
     "be_petty": True,
+    "nuclear": 0,
     "petty_counter": 1,
     "petty_cap": 3,
     "iterations": 0,
@@ -39,23 +35,17 @@ def save_file(file):
         with open(opp_moves_history, "w") as f:
             json.dump(opponent_history, f)
 
-
-
 def load_file(file):
-
-    
+  
     if not exists(file):
         save_file(file)
     with open(file) as f:
         return json.load(f)
 
-
-
 def print_data():
 
-    print("\nParam File")
+    print("\nParameter File")
     print(parameters)
-
     print("\n-Opp History-")
     print(opponent_history)
     print("\n")
@@ -68,15 +58,11 @@ if __name__ == "__main__":
     parser.add_argument('--iterations', help='number of iterations in game')
     parser.add_argument('--last_opponent_move', help='last opponent move')
     args = parser.parse_args()
-
    
     is_new_game = args.init
     iterations = args.iterations
     opponents_last_move = args.last_opponent_move
-
-
-
-   
+  
     if is_new_game is not None:
         save_file(parameter_storage)
         save_file(opp_moves_history)
@@ -84,7 +70,6 @@ if __name__ == "__main__":
        
         parameters = load_file(parameter_storage)
         opponent_history = load_file(opp_moves_history)
-
    
     try:
         opponent_history["history"] += opponents_last_move[0]
@@ -103,12 +88,14 @@ if __name__ == "__main__":
 
     
     if opponents_last_move == "confess":
+        parameters["nuclear"] += 1
 
        
-        if parameters["anger"]:
+        if parameters["nuclear"] > 10:
+         print("confess")
 
            
-            parameters["chip_counter"] < parameters["grudgelength"]
+        if parameters["chip_counter"] < parameters["grudgelength"]:
             print("confess")
 
        
@@ -118,23 +105,28 @@ if __name__ == "__main__":
        
         if parameters["be_petty"]:
 
-            if parameters["spill_the_beans_odds"] >= parameters["petty_cap"]:
+            if parameters["spill_the_beans_odds"] <= parameters["petty_cap"]:
+               print("silent")
 
-                parameters["spill_the_beans_odds"] -= parameters["petty_counter"]
-
+            if parameters["spill_the_beans_odds"] >= parameters["petty_counter"]:
+                print("confess")
    
     else:
 
         
-        if parameters["iterations"] < 5:
+        if parameters["iterations"] < 3:
 
                 r  = random.randint(0,5)
 
                 if r == 0:
                     print("confess")
-                 
+                  
     
-       
+                if parameters["iterations"] > 97:
+                    b = random.randint(1,10)
+                    if b == 10:
+                        print("confess")
+           
         else:
 
            
@@ -149,18 +141,18 @@ if __name__ == "__main__":
 
                 
                 if parameters["spill_the_beans"]:
-                                                      
+                
+                
                         t = random.randint(0,10)
                       
                         if t == 0:
-                            print("silent")
+                            print("confess")
                                          
-                    
+                        else:
+                            print("silent")
 
                     
-                    except ValueError:
-
-                        print("silent")
+                        
 
                 
                 else:
