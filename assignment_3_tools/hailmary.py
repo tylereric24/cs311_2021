9525eb58c7c9757596bedd89d793c5862b13cade
Eric Tyler <83565207+tylereric24@users.noreply.github.com>
2021-11-21 11:27:10 -0800

    Update hailmary.py

--- assignment_3_tools/hailmary.py
+++ assignment_3_tools/hailmary.py
@@ -1,74 +1,63 @@
 import random
 import string
-import itertools
-from textwrap import indent
 
-NODE_COUNT_PER_LAYER = [4,3,2]
-
+NODE_PER_LAYER = [4,3,2] #Global variable/list
+#class holds data and methods self=this
 class Node:
-    def __init__(self):
-        self.children = [] #connection to children
-        self.weight = [] #weight of connection to children
+  def __init__(self):
+    self.children = [] #connection to list of children
+    self.weight = [] #list of weight of connection to children
+    self.name = ''.join([random.choice(string.ascii_letters) for i in range(3)]) #set a random name to each Node
 
-        self.node_name = ''
-        random_letters = []
-        for i in range (3):
-            random_letters.append( random.choice(string.ascii_letters) )
-        self.node_name = '' .join(random_letters)
+  def make_children(self, current_layer_number, node_per_layer_map):
+    #base case
+    if current_layer_number >= len(node_per_layer_map):
+      return
+    #create all children for this layer
+    for i in range( node_per_layer_map[current_layer_number] ):
+      self.children.append( Node() )
+    #recursively connect children for next layers
+    self.children[0].make_children( current_layer_number + 1, node_per_layer_map )
+    #copy all children from [0] to all other chldren (deep copy)
+    for i in range(1, len(self.children) ):
+      self.children[i].children = self.children[0].children[:]
 
-        #''.join([random.choice(string.ascii_letters) for i in range(3)])
-
-    def make_children(self, current_layer_number, node_per_layer_map):
-        #When to terminate
-        if current_layer_number >= len(node_per_layer_map):
-            return
-
-        for i in range( node_per_layer_map[current_layer_number] ):
-            self.children.append( Node() )
-
-        #self.children area all node children for level
-        self.children[0].make_children( current_layer_number + 1, node_per_layer_map)
-
-        #copy all children from [0] to all other children
-        for i in range(1, len(self.children) ):
-            self.children[i].children = self.children[0].children[:]
+  def print_layers(self, current_layer_number, node_per_layer_map):
+    indent = '    ' * current_layer_number #makes output look nice
+    #base case
+    if current_layer_number >= len(node_per_layer_map):
+      print(f"{indent} {self.name}")
+      return
+    #so output makes sense
+    print(f"{indent} {self.name} is connected to:")
+    for i in range(len(self.children) ):
+      try:
+        print(f"{indent} Weight of {self.weight[i]}")
+      except:
+        pass
+      #recurse
+      self.children[i].print_layers(current_layer_number + 1, node_per_layer_map)
     
-    def prety_print(self, current_layer_number, node_per_later_map):
-        indent = '   ' - current_layer_number
-
-        #When to stop calling self
-        if current_layer_number >= len(node_per_layer_map):
-            print(f"{indent} {self.node_name}")
-            return
-
-        print(f"{indent} {self.node_name} is connected to:")
-        for i in range( len(self.children) ):
-            try:
-                print(f"{indent} Weight of {self.weight[i]}")
-            except:
-                pass
-
-            self.children[i].prety_print(current_layer_number + 1, node_per_layer_map)
-
-        return
-
-    def set_random_weights(self, current_layer_number, node_per_layer_map):
-        #when to stop calling self
-        if current_layer_number >= len(node_per_layer_map):
-            return
-    self.weight = [0.0] = len(self.children)
-    for i in range( len(self.children) ):
-        self.weight[i] = random.uniform(0,1)
-        self.children[i].set_random_weights(current_layer_number + 1, node_per_layer_map)
-
     return
 
-new_node = node()
-new_node.make_children(0,NODE_COUNT_PER_LAYER)
-#new_node.prety_print(0, NODE_COUNT_PER_LAYER)
-#print("AFTER weights")
-new_node.set_random_weights(0, NODE_COUNT_PER_LAYER)
+  def weights(self, current_layer_number, node_per_layer_map):
+    #base case
+    if current_layer_number >= len(node_per_layer_map):
+      return
+    #initialize
+    self.weight = [0,0] * len(self.children)
+    #set random weights
+    for i in range( len(self.children) ):
+      self.weight[i] = random.uniform(0, 1)
+      #recurse
+      self.children[i].weights(current_layer_number + 1, node_per_layer_map)
 
-new_node.prety_print(0, NODE_COUNT_PER_LAYER)
-
-print(list(itertools.product([0,1], retreat=4)))
+    return
+#master node
+new_node = Node()
+#create all the children
+new_node.make_children(0, NODE_PER_LAYER)
+#set all the weights
+new_node.weights(0, NODE_PER_LAYER)
+#output the results
+new_node.print_layers(0, NODE_PER_LAYER)
