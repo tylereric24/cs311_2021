28702cac290524e78a5a08d464d0937827b96b2c
Eric Tyler <83565207+tylereric24@users.noreply.github.com>
2021-11-21 11:52:29 -0800

    Update hailmary.py

--- assignment_3_tools/hailmary.py
+++ assignment_3_tools/hailmary.py
@@ -1,32 +1,28 @@
 import random
 import string
 
-NODE_PER_LAYER = [4,3,2]
-
-RANDOM_NODE_NAMES = ["Kona", "Riley", "Nahla", "Otis", "Templeton", "Gilligan", "Swiss", "Cooper", "Murphy", "Bristol", "Brady", "Stitch", "Charlie", "Diesel",
-"Benji", "Archie", "Chi"]
-
+NODE_PER_LAYER = [4,3,2] 
 class Node:
   def __init__(self):
     self.children = [] 
     self.weight = [] 
-    self.node_name = ''.join([random.choice(string.ascii_letters) for i in range(3)]) 
-
+    self.name = ''.join([random.choice(string.ascii_letters) for i in range(3)]) 
+    
   def make_children(self, current_layer_number, node_per_layer_map):
-   
+    
     if current_layer_number >= len(node_per_layer_map):
       return
-    
+   
     for i in range( node_per_layer_map[current_layer_number] ):
       self.children.append( Node() )
-   
+  
     self.children[0].make_children( current_layer_number + 1, node_per_layer_map )
    
     for i in range(1, len(self.children) ):
       self.children[i].children = self.children[0].children[:]
 
   def print_layers(self, current_layer_number, node_per_layer_map):
-    indent = '    ' * current_layer_number 
+    indent = '    ' * current_layer_number
    
     if current_layer_number >= len(node_per_layer_map):
       print(f"{indent} {self.name}")
@@ -38,21 +34,21 @@ class Node:
         print(f"{indent} Weight of {self.weight[i]}")
       except:
         pass
-      
+     
       self.children[i].print_layers(current_layer_number + 1, node_per_layer_map)
     
     return
 
   def weights(self, current_layer_number, node_per_layer_map):
-    
+   
     if current_layer_number >= len(node_per_layer_map):
       return
-   
+  
     self.weight = [0,0] * len(self.children)
    
     for i in range( len(self.children) ):
       self.weight[i] = random.uniform(0, 1)
-      
+     
       self.children[i].weights(current_layer_number + 1, node_per_layer_map)
 
     return
